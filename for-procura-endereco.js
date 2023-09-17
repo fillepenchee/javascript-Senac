888b3e0b152cd6ccffe03d07e218d747deba2c7a
Simone Campos <simonecampos@gmail.com>
2023-09-16 21:24:48 -0300

    Add files via upload

---
+++ for-procura-endereco.js
@@ -0,0 +1,9 @@
+// loop FOR - procurando endereço
+
+console.log("Entramos na Rua das Marrecas. Procurando o endereço...");
+
+for (var numerocasa = 1; numerocasa < 21; numerocasa = numerocasa + 2) {
+console.log("Passando em frente ao número " + numerocasa + "...");
+}
+
+console.log("Você achou o endereço! Rua das Marrecas, 21");
